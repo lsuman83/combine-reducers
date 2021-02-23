7fd38b4a22206dc6e938444e8ea2495c92cf48bb
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-02-22 20:57:38 -0500

    Done.

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,54 @@
+import { combineReducers} from 'redux'
+import uuid from 'uuid';
+
+const rootReducer = combineReducers({
+    authors: authorsReducer,
+    books: booksReducer
+})
+
+
+export default rootReducer
+
+
+function booksReducer(state = [], action) {
+    let idx;
+
+    switch (action.type) {
+        case 'ADD_BOOK':
+            return [...state, action.book];
+
+        case 'REMOVE_BOOK':
+            idx = state.findIndex(book => book.id === action.id)
+            return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+        default:
+            return state;
+    }
+}
+
+function authorsReducer(state = [], action) {
+    let idx;
+
+    switch (action.type) {
+        case 'ADD_AUTHOR':
+            return [...state, action.author];
+        
+        case 'REMOVE_AUTHOR':
+            idx = state.findIndex(author => author.id === action.id)
+            return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+        case 'ADD_BOOK':
+            let existingAuthor = state.filter(suthor =>
+                suthor.aukthorName === action.book.authorName);
+            
+            if (existingAuthor.length > 0) {
+                return state;
+            } else {
+                return [...state, {authorName: action.book.authorName, id: uuid()}];
+            }
+            
+        default:
+            return state;
+    }
+}
+
